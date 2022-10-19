68d83c511366c7cf0b43e9f7803e208383e841d2
Remy Lagrois <rlagrois@oreilly.com>
2022-10-18 21:38:47 -0400

    create base digit map dynamically

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,10 +1,15 @@
 package module01
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
+//
+// What is written is capable of up to base 36
 //
 // Eg:
 //
@@ -13,12 +18,8 @@ import "strings"
 
 func DecToBase(dec, base int) string {
 	var conv string
-	hex := map[int]string{
-		0: "0", 1: "1", 2: "2", 3: "3",
-		4: "4", 5: "5", 6: "6", 7: "7",
-		8: "8", 9: "9", 10: "A", 11: "B",
-		12: "C", 13: "D", 14: "E", 15: "F",
-	}
+
+	hex := CreateBaseMap(base)
 
 	for dec != 0 {
 		digit := dec % base
@@ -46,4 +47,20 @@ func DecToBaseSbRev(dec, base int) string {
 	}
 
 	return Reverse(sb.String())
+}
+
+func CreateBaseMap(base int) map[int]string {
+	const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	cmap := map[int]string{}
+
+	for i := 0; i < base; i++ {
+		if i < 10 {
+			cmap[i] = strconv.Itoa(i)
+		} else {
+			ind := i - 10
+			cmap[i] = string(alpha[ind])
+		}
+	}
+
+	return cmap
 }
