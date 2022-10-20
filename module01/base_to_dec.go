77da2902322abb2cf636f149452949b15f4bbe9e
Remy Lagrois <rlagrois@oreilly.com>
2022-10-19 20:51:55 -0400

    base to dec

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,13 +1,39 @@
 package module01
 
+import (
+	"math"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
 // Eg:
 //
-//   BaseToDec("E", 16) => 14
-//   BaseToDec("1110", 2) => 14
-//
+//	BaseToDec("E", 16) => 14
+//	BaseToDec("1110", 2) => 14
 func BaseToDec(value string, base int) int {
-	return 0
+	sum := 0
+	revHex := reverseHexMap(base)
+	revVal := Reverse(value)
+	places := len(value)
+
+	for i := places - 1; i >= 0; i-- {
+		degree := math.Pow(float64(base), float64(i))
+		mapKey := string(revVal[i])
+		mult := int(degree) * revHex[mapKey]
+		sum += mult
+	}
+
+	return sum
+}
+
+func reverseHexMap(base int) map[string]int {
+	revHex := map[string]int{}
+	hex := CreateBaseMap(base)
+
+	for key, element := range hex {
+		revHex[element] = key
+	}
+
+	return revHex
 }
