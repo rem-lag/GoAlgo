1a2f89ddca4d021d0af62bc0c55d86428a5bb244
Remy Lagrois <rlagrois@oreilly.com>
2022-10-21 23:30:37 -0400

    factor

--- module01/factor.go
+++ module01/factor.go
@@ -11,16 +11,31 @@ package module01
 //
 // Examples:
 //
-//   Factor([]int{2,3,5}, 30) // []int{2,3,5}
-//   Factor([]int{2,3,5}, 28) // []int{2,2,7}
-//   Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
+//	Factor([]int{2,3,5}, 30) // []int{2,3,5}
+//	Factor([]int{2,3,5}, 28) // []int{2,2,7}
+//	Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
 //
 // Examples with remainders:
 //
-//   Factor([2,5], 30) // []int{2,5,3}
-//   Factor([3,5], 720) // []int{3,3,5,16}
-//   Factor([], 4) // []int{4}
-//
+//	Factor([2,5], 30) // []int{2,5,3}
+//	Factor([3,5], 720) // []int{3,3,5,16}
+//	Factor([], 4) // []int{4}
 func Factor(primes []int, number int) []int {
-	return nil
+	facts := []int{}
+
+	for number != 1 {
+		cnum := number
+		for _, p := range primes {
+			if number%p == 0 {
+				facts = append(facts, p)
+				number = number / p
+			}
+		}
+		if cnum == number {
+			facts = append(facts, number)
+			number = 1
+		}
+	}
+
+	return facts
 }
