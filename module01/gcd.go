48fd5488952256128f408ea233da373a2c94acd5
Remy Lagrois <rlagrois@oreilly.com>
2022-10-22 22:59:26 -0400

    gcd

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,35 @@
 package module01
 
+import "math"
+
 func GCD(a, b int) int {
-	return 0
+	primes := returnPrimes()
+	af := Factor(primes, a)
+	bf := Factor(primes, b)
+	ac, bc := make(map[int]int), make(map[int]int)
+	gcd := 1
+
+	for _, val := range af {
+		ac[val]++
+	}
+	for _, val := range bf {
+		bc[val]++
+	}
+	for key, aval := range ac {
+		if bval, ok := bc[key]; ok {
+			min := math.Min(float64(aval), float64(bval))
+			add := key * int(min)
+			gcd *= add
+		}
+	}
+
+	return gcd
+}
+
+func GCDE(a, b int) int {
+	if b == 0 {
+		return a
+	} else {
+		return GCD(b, a%b)
+	}
 }
